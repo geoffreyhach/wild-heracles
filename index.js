ec9941709947920e2ed369f129b3411033dc298a
Geoffrey Hach <je.suis.geoffrey.hach@gmail.com>
2022-10-06 14:36:50 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require("./src/Fighter").Fighter;
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const nemeanLion = new Fighter("🦁 Nemean Lion", 11, 13);
+
+const geoffrey = new Fighter("🕺 Geoffrey", 20, 15);
+const yavuz = new Fighter("👴 Yavuz", 2, 1);
+
+// console.log(`${heracles.name} has 💙 ${heracles.life}HP`);
+// console.log(`${nemeanLion.name} has 💙 ${nemeanLion.life}HP`);
+
+heracles.fight(nemeanLion);
+
+// geoffrey.fight(yavuz);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,62 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor(name, str, dex, life = MAX_LIFE) {
+        this.name = name;
+        this.str = str;
+        this.dex = dex;
+        this.life = life;
+    }
+
+    fight(ennemy) {
+        console.log(`${this.name} is attacking ${ennemy.name}`);
+
+        let i = 1;
+        //as long as a fighter is alive we keep launching a round of attack
+        while (isAlive(ennemy) && isAlive(this)) {
+            console.log(`🕛 Round #${i}`);
+            attack(this, ennemy);
+            if (isAlive(ennemy)) attack(ennemy, this); //if the enemy is dead, he can't attack back during the round
+            i++;
+        }
+        displayFightResult(this, ennemy);
+    }
+}
+
+function attack(attacker, defender) {
+    defender.life -= damageDealt(attacker, defender);
+    displayAttackResult(attacker, defender);
+}
+
+// use to calculate the attackers damage based on his strenght and the damage received by defender base on his dexterity
+function damageDealt(attacker, defender) {
+    let damage = Math.round(Math.random() * (attacker.str - 1) + 1);
+    let damageDealt = damage - defender.dex;
+
+    if (damageDealt <= 0) {
+        return 0;
+    }
+    if (damageDealt > 0) {
+        return damageDealt;
+    }
+}
+
+function isAlive(character) {
+    if (character.life > 0) return true;
+    else return false;
+}
+
+function displayFightResult(attacker, defender) {
+    if (isAlive(attacker)) console.log(`${attacker.name} won`);
+    if (isAlive(defender)) console.log(`${defender.name} won`);
+}
+
+function displayAttackResult(attacker, defender) {
+    console.log(
+        `${attacker.name} 🗡️ ${defender.name}, 💙${defender.name} = ${defender.life} `
+    );
+}
+
+exports.Fighter = Fighter;
